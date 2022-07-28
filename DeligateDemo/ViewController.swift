2ac84549576c9d8ebb95346c1899c5434a603cf4
Naidu <naidup.developer@gmail.com>
2022-07-28 17:19:12 +0530

    Initial Commit

---
+++ DeligateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeligateDemo
+//
+//  Created by Chanti on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
